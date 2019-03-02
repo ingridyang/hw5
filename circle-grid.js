6efc5e8e7e0a95d7f350078f9208888fc6b8005b
ingridyang <43019677+ingridyang@users.noreply.github.com>
2019-03-01 23:29:47 -0800

    Create circle-grid.js

---
+++ circle-grid.js
@@ -0,0 +1,23 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(20);
+  for (var i = 10; i <= width; i = i + 50) {
+    ellipse(i, 20, 25, 25);
+    ellipse(i, 50, 25, 25);
+    ellipse(i, 80, 25, 25);
+    ellipse(i, 110, 25, 25);
+    ellipse(i, 140, 25, 25);
+    ellipse(i, 170, 25, 25);
+    ellipse(i, 200, 25, 25);
+    ellipse(i, 230, 25, 25);
+    ellipse(i, 260, 25, 25);
+    ellipse(i, 290, 25, 25);
+    ellipse(i, 320, 25, 25);
+    ellipse(i, 350, 25, 25);
+    ellipse(i, 380, 25, 25);
+	
+  }
+}
